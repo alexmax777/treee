ead9036a22274bc3639cb9664bde941e8d9559be
alexmax777 <gpapyan1987@gmail.com>
2021-02-11 20:05:33 +0400

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+ hello from vue
+    </div>
+</template>
+
+
+
+<script>
+
+export default {
+
+ data(){
+     return{
+         posts: [],
+         post: {
+             id: '',
+             title: '',
+             content: '',
+         }
+
+     }
+
+ },
+    mounted() {
+        axios
+            .get('/api/posts')
+            .then(response => {
+                this.posts = response.data.data
+            })
+            .catch(error => {
+                console.log(error)
+                this.errored = true;
+            })
+            .finally(() => this.loading = false)
+    }
+
+}
+</script>
+
